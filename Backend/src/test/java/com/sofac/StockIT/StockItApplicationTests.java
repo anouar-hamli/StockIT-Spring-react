bce0976f576131f21a87a54f218e320cf3bda0b2
anouar hamli <anouarhamli10@gmail.com>
2025-06-03 10:47:11 +0100

    first commit

---
+++ Backend/src/test/java/com/sofac/StockIT/StockItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofac.StockIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
